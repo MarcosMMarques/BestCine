bb46fdd902024a6dda4503d091996518104ceee7
Marcos Marques <mdmarques70@gmail.com>
2025-10-03 13:13:25 -0300

    create: reservation_seat table

---
+++ database/migrations/2025_10_03_160947_create_reservation_seats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservation_seat', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('seat_id')->constrained(
+                table: 'seat'
+            );
+
+            $table->foreignId('reservation_id')->constrained(
+                table: 'reservation'
+            );
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservation_seat');
+    }
+};
