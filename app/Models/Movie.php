959ad43b5525d9ad89149ce982ca015b6b795bd9
Marcos Marques <mdmarques70@gmail.com>
2025-10-03 15:01:25 -0300

    add: Movie model relations

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany
+use App\Models\Session;
 
 class Movie extends Model
 {
@@ -18,4 +20,9 @@ class Movie extends Model
     protected $hidden = [
         'image',
     ];
+
+    protected function sessions()
+    {
+        return $this->hasMany(Sessiom::class);
+    }
 }
