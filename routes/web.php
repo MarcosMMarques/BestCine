1f8501f4936b48c0644a6fa3ea1be603c64fa3d6
Marcos Marques <mdmarques70@gmail.com>
2025-10-06 15:27:01 -0300

    add: main page route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/', [MovieController::class, 'index']);
