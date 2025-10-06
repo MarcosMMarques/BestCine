045bf23c04ff88ca3fab84c3b0913f9ccd21c73f
Marcos Marques <mdmarques70@gmail.com>
2025-10-06 15:13:51 -0300

    add: Room model relations

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -3,6 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+<<<<<<< Updated upstream
+=======
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Session;
+use App\Models\Seat;
+>>>>>>> Stashed changes
 
 class Room extends Model
 {
@@ -12,4 +18,17 @@ class Room extends Model
         'name',
         'seat_quantity',
     ];
+<<<<<<< Updated upstream
+=======
+
+    protected function sessions(): HasMany
+    {
+        return $this->hasMany(Session::class);
+    }
+
+    protected function seats(): HasMany
+    {
+        return $this->hasMany(Seat::class);
+    }
+>>>>>>> Stashed changes
 }
