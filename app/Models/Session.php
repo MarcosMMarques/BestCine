51fe0b550e150150506765217c2f481a274bcfa6
Marcos Marques <mdmarques70@gmail.com>
2025-10-06 15:14:05 -0300

    add: Session model relations

--- app/Models/Session.php
+++ app/Models/Session.php
@@ -3,6 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Movie;
+use App\Models\Room;
+use App\Models\Reservation;
 
 class Session extends Model
 {
@@ -13,4 +18,19 @@ class Session extends Model
         'movie_id',
         'datetime'
     ];
+
+    protected function movie(): BelongsTo
+    {
+        return $this->belongsTo(Movie::class);
+    }
+
+    protected function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    protected function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }
