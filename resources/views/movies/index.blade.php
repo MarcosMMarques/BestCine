08b2f80cac1923986039a11f89671b9a8907b73c
Marcos Marques <mdmarques70@gmail.com>
2025-10-07 23:05:38 -0300

    style: improve movie component using tailwindcss

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,28 +1,22 @@
+@extends('layouts.app')
 
-<section class="bg-gray-900 text-white py-12">
+@section('content')
   <div class="container mx-auto px-4">
 
-    <h2 class="text-3xl font-bold text-center mb-8">Now Showing</h2>
+    <h2 class="text-3xl font-bold text-center mb-8">Em cartaz</h2>
 
     @if($movies)
       <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
 
         @foreach ($movies as $movie)
-          <div class="bg-gray-800 rounded-lg overflow-hidden shadow-lg group transform transition-transform duration-300 hover:-translate-y-2">
-            <a href="#">
-              <img
-                src="https://image.tmdb.org/t/p/w500{{$movie['poster_path']}}"
-                alt="Poster of {{ $movie['title']}}"
-                class="w-full h-auto object-cover aspect-[2/3] transform group-hover:scale-105 transition-transform duration-300"
-              >
-            </a>
+            <div class="w-full max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                <img class="object-cover w-full h-56" src="https://image.tmdb.org/t/p/w500{{$movie['poster_path']}}" alt="avatar">
 
-            <div class="p-4">
-              <h3 class="text-lg font-semibold truncate" title="{{ $movie['title']}}">
-                {{ $movie['title']}}
-              </h3>
+                <div class="py-5 text-center">
+                    <a href="#" class="block text-xl font-bold text-gray-800 dark:text-white" tabindex="0" role="link">{{ $movie['title'] }}</a>
+                    <span class="text-sm text-gray-700 dark:text-gray-200">{{ $movie['release_date'] }}</span>
+                </div>
             </div>
-          </div>
         @endforeach
 
       </div>
@@ -33,5 +27,4 @@
     @endif
 
   </div>
-</section>
-
+@endsection
