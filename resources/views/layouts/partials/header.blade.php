04b62474095661ab6e19741eff2de98af0e55816
Marcos Marques <mdmarques70@gmail.com>
2025-10-13 14:23:12 -0300

    style: change the app icon on header of main layout

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -2,8 +2,8 @@
   <nav aria-label="Global" class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
     <div class="flex lg:flex-1">
       <a href="#" class="-m-1.5 p-1.5">
-        <span class="sr-only">Your Company</span>
-        <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="" class="h-8 w-auto" />
+        <span class="sr-only">Best Cine</span>
+        <img src="{{ asset('images/icon.png') }}" alt="Logotipo Best Cine" class="h-10 w-auto" />
       </a>
     </div>
     <div class="flex lg:hidden">
