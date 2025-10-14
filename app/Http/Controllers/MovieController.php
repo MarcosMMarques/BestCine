14bf99bd27e27235305d69ac0c68c248f6f4d1bd
Marcos Marques <mdmarques70@gmail.com>
2025-10-14 10:49:07 -0300

    create: action show on MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Services\TmdbService;
 
 class MovieController extends Controller
@@ -19,5 +18,16 @@ class MovieController extends Controller
         $movies = $this->tmdb->getNowShowingMoviesInBrazil(1)['results'];
         // dd($movies);
         return view('movies.index', ['movies' => $movies]);
+    }
+
+    public function show(int $movieId)
+    {
+        $movie = $this->tmdb->getMovieDetails($movieId);
+
+        if (empty($movie) || (isset($movie['success']) && $movie['success'] === false)) {
+            abort(404);
+        }
+
+        return view('movies.show', ['movie' => $movie]);
     }
 }
