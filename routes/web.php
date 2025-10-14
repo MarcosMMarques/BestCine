207475e4910eb1a7c809aad37551be28afa83481
Marcos Marques <mdmarques70@gmail.com>
2025-10-14 10:46:55 -0300

    create: movie show route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
-Route::get('/', [MovieController::class, 'index']);
+Route::get('/', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/movies/{movieId}', [MovieController::class, 'show'])
+    ->whereNumber('movieId')
+    ->name('movies.show');
