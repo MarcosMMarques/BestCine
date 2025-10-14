5105b7f315576be7e1f730369cf90fb9d22e7541
Marcos Marques <mdmarques70@gmail.com>
2025-10-14 11:57:14 -0300

    feat: get cast and trailer in show movie action

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -22,12 +22,29 @@ class MovieController extends Controller
 
     public function show(int $movieId)
     {
-        $movie = $this->tmdb->getMovieDetails($movieId);
+        $movie = $this->tmdb->getMovieDetails($movieId, ['videos', 'credits']);
 
         if (empty($movie) || (isset($movie['success']) && $movie['success'] === false)) {
             abort(404);
         }
 
-        return view('movies.show', ['movie' => $movie]);
+        $trailer = collect(data_get($movie, 'videos.results', []))
+            ->first(function ($video) {
+                return ($video['type'] ?? null) === 'Trailer'
+                    && ($video['site'] ?? null) === 'YouTube'
+                    && filled($video['key'] ?? null);
+            });
+
+        $cast = collect(data_get($movie, 'credits.cast', []))
+            ->filter(function ($actor) {
+                return filled($actor['name'] ?? null) || filled($actor['character'] ?? null);
+            })
+            ->take(10);
+
+        return view('movies.show', [
+            'movie' => $movie,
+            'trailer' => $trailer,
+            'cast' => $cast,
+        ]);
     }
 }
