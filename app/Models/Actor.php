3a5b0d351d1bf4f154a94319a04de26feb744c21
Marcos Marques <mdmarques70@gmail.com>
2025-10-22 16:38:16 -0300

    create: Actor Model

---
+++ app/Models/Actor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Actor extends Model
+{
+    protected $table = 'actor';
+
+    protected $incrementing = false;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'profile_path',
+    ];
+
+
+    public function movies(): BelongsToMany
+    {
+        return $this->belongsToMany(Movie::class, 'actor_movie', 'actor_id', 'movie_id');
+    }
+}
