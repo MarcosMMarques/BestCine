dc331db62be040fe0c48ec8d479f56c68b268c8e
Marcos Marques <mdmarques70@gmail.com>
2025-10-27 20:35:23 -0300

    refactor: add extra columns in Movie model

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use App\Models\Session;
 
 class Movie extends Model
@@ -15,14 +16,34 @@ class Movie extends Model
         'synopsis',
         'length',
         'image',
+        'backdrop_url',
+        'poster_url',
+        'trailer_url',
+        'tagline',
+        'release_date',
     ];
 
     protected $hidden = [
         'image',
     ];
 
-    protected function sessions()
+    protected function sessions(): HasMany
     {
         return $this->hasMany(Session::class);
+    }
+
+    protected function genres(): BelongsToMany
+    {
+        return $this->belongsToMany(Genre::class, 'movie_genre', 'movie_id', 'genre_id');
+    }
+
+    protected function actors(): BelongsToMany
+    {
+        return $this->belongsToMany(Actor::class, 'actor_movie', 'movie_id', 'actor_id');
+    }
+
+    protected function productionCompanies(): BelongsToMany
+    {
+        return $this->belongsToMany(ProductionCompanie::class, 'movie_production_companie', 'movie_id', 'production_companie_id');
     }
 }
