593a36c08a4917b9a22a1b4eb091969a751205de
Marcos Marques <mdmarques70@gmail.com>
2026-01-25 14:36:10 -0300

    create: RoomFactory

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\Session;
@@ -9,6 +10,8 @@ use App\Models\Seat;
 
 class Room extends Model
 {
+    use HasFactory;
+
     protected $table = 'room';
 
     protected $fillable = [
