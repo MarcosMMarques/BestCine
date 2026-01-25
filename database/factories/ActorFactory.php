ff2276e0b87d1ca7b918a4596e19353e4137665a
Marcos Marques <mdmarques70@gmail.com>
2026-01-25 14:31:09 -0300

    create: ActorFactory

---
+++ database/factories/ActorFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Actor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Actor>
+ */
+class ActorFactory extends Factory
+{
+    protected $model = Actor::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => fake()->unique()->numberBetween(1000, 999999),
+            'name' => fake()->name(),
+            'profile_path' => '/path/to/profile/' . fake()->uuid() . '.jpg',
+        ];
+    }
+}
