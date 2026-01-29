2e2349bea7ae27c18388ac1c7c618ec4a41040ff
Marcos Marques <mdmarques70@gmail.com>
2026-01-28 22:57:36 -0300

    test: throws SeatAlreadyReservedException if seat is already reserved

---
+++ tests/Unit/Reservation/ReservationServiceValidationTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Services\ReservationService;
+use App\Exceptions\SeatAlreadyReservedException;
+use App\Exceptions\UserAlreadyHasReservationException;
+use App\Models\User;
+use App\Models\Movie;
+use App\Models\Seat;
+use Illuminate\Support\Carbon;
+
+it('throws SeatAlreadyReservedException if seat is already reserved', function () {
+    $service = Mockery::mock(ReservationService::class)->makePartial();
+
+    $service
+        ->shouldReceive('checkSeatReservationByMovieAndDateTime')
+        ->once()
+        ->andReturn(true);
+
+    $service
+        ->shouldReceive('checkIfUserHasReservationForSession')
+        ->never();
+
+    expect(
+        fn () =>
+        $service->validateReservation(
+            Mockery::mock(User::class),
+            Mockery::mock(Movie::class),
+            Carbon::now(),
+            Mockery::mock(Seat::class)
+        )
+    )->toThrow(SeatAlreadyReservedException::class);
+});
