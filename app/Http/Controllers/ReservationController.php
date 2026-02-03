2f5b4cb809de9b730f2bb53ddacf3e5e00863c73
Marcos Marques <mdmarques70@gmail.com>
2026-02-02 22:02:56 -0300

    refactor: checkout action to deal with mutiple seats

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Seat;
 use App\Models\Session;
 use App\Services\ReservationService;
-use Exception;
 use Illuminate\Http\Request;
 use App\Models\Movie;
 use App\Payment\PaymentGatewayInterface;
 use Illuminate\Support\Carbon;
 use App\Exceptions\SeatAlreadyReservedException;
 use App\Exceptions\UserAlreadyHasReservationException;
+use App\Http\Requests\CheckoutRequest;
 
 class ReservationController extends Controller
 {
     protected $paymentGateway;
+    protected $reservationService;
 
     public function __construct(
         PaymentGatewayInterface $paymentGateway,
@@ -25,24 +26,29 @@ class ReservationController extends Controller
         $this->reservationService = $reservationService;
     }
 
-    public function checkout(Request $request, Movie $movie)
+    public function checkout(CheckoutRequest $request, Movie $movie)
     {
-        //TODO: Validar Dados
-        $date = Carbon::parse($request['session'], config('app.timezone'));
-        $seat = Seat::find(1);
+        $validated = $request->validated();
+        $date = Carbon::parse($validated['session'], config('app.timezone'));
+        $seats = Seat::getSeatsByIdentifiers($validated['seats']);
+
+        if (count($seats) == 0) {
+            return redirect()->route('movies.sessions', compact('movie'))
+                ->with('error', 'Pelo menos um assento válido precisa ser selecionado.');
+        }
 
         try {
             $this->reservationService->validateReservation(
                 $request->user(),
                 $movie,
                 $date,
-                $seat
+                $seats
             );
 
             $metadata = [
                 'user_id' => (string) $request->user()->id,
                 'movie_id' => (string) $movie->id,
-                'seat_id' => (string) $seat->id,
+                'seat_ids' => collect($seats)->pluck('id'),
                 'session_datetime' => $date->toIso8601String(),
             ];
             $successUrl = route('reservation.success');
@@ -51,12 +57,11 @@ class ReservationController extends Controller
 
             return redirect()->away($session->url);
         } catch (SeatAlreadyReservedException | UserAlreadyHasReservationException $e) {
-            return response()->json([
-                'message' => $e->getMessage()
-            ], 409);
+            return redirect()->route('movies.sessions', compact('movie'))
+                ->with('error', $e->getMessage());
         } catch (\Exception $e) {
-            dd($e);
-            return redirect()->route('movies.sessions', compact('movie'))->with('error', $e->getMessage());
+            return redirect()->route('movies.sessions', compact('movie'))
+                ->with('error', 'Ocorreu um erro ao processar sua reserva. Tente novamente.');
         }
     }
 
